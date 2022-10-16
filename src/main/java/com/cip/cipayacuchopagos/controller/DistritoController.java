dd2cea3e9e66c677bc302065403c99c44398abc4
carangop <109491792+carangop@users.noreply.github.com>
2022-10-16 00:55:00 -0500

    prueba

--- src/main/java/com/cip/cipayacuchopagos/controller/DistritoController.java
+++ src/main/java/com/cip/cipayacuchopagos/controller/DistritoController.java
@@ -6,26 +6,18 @@
 package com.cip.cipayacuchopagos.controller;
 
 import com.cip.cipayacuchopagos.entity.Distrito;
-import com.cip.cipayacuchopagos.entity.TipoDocumento;
-import com.cip.cipayacuchopagos.repository.impl.CapituloRepositoryImpl;
-import com.cip.cipayacuchopagos.service.CapituloService;
 import com.cip.cipayacuchopagos.service.DistritoService;
-import lombok.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.List;
 /**
  *
  * @author Arango
